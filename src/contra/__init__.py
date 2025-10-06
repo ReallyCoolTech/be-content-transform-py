bc764583939c55818516b6f4caca537fc9de24d5
DanHugoDanHugo <dhugo@reallycool.com>
2025-10-06 13:34:02 -0700

    feat: add hex architecture skeleton
    Signed-off-by: DanHugoDanHugo <dhugo@reallycool.com>

---
+++ src/contra/__init__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2025 ReallyCool Technologies, LLC
+#
+# This file is part of be-content-transform-py.
+#
+# be-content-transform-py is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# be-content-transform-py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with be-content-transform-py.  If not, see <https://www.gnu.org/licenses/>.
+
+def main() -> None:
+    print("Hello from contra!")
